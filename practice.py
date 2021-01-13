5231bf0667d9986a3a3e008d6919fabeddbe5bc7
Tab Gui <49506575+TabGuigui@users.noreply.github.com>
2021-01-13 16:22:55 +0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -25,7 +25,34 @@ class Solution:
             else:
                 hashmap[nums[i]] = i
 
-                
+# 2题 链表相加
+# 思路 比较简单 注意创建链表之后的操作需要用新变量替代进行操作,否则一直是空链表
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        re = ListNode(0)
+        r = re
+        carry = 0
+        while(l1 or l2):
+            a = l1.val if l1 else 0
+            b = l2.val if l2 else 0
+            s = a + b + carry
+            carry = s//10
+            r.next = ListNode(s%10)
+            print(re)
+            print(r)
+            r = r.next
+            if(l1!=None):
+                l1 = l1.next
+            if(l2!=None):
+                l2 = l2.next
+        if(carry>0):
+            r.next = ListNode(1)
+        return re.next
                 
 # 动态规划部分
 
