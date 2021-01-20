ca42aa23a612452d38d0b2b2ec25b294a154b6f4
TabGuigui <gui623344308@qq.com>
2021-01-20 10:02:07 +0800

    1.20

--- Jan.py
+++ Jan.py
@@ -6,6 +6,26 @@ Time             :2021/01/19 16:07:46
 Author           :tab gui
 Version          :1.0
 '''
+# 第三题 无重复字符的最长子串
+# 用滑动窗口法 保存滑动过的窗口，新字符如果在该窗口内，应减小窗口范围
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        print(s)
+        if len(s) == 0:
+            return 0
+        max_len = 0
+        cur_len = 0
+        window = ''
+        for i in range(len(s)):
+            cur_len += 1
+            while s[i] in window:
+                cur_len -= 1
+                window = window[1:]
+            if cur_len > max_len:
+                max_len = cur_len
+            window += s[i]
+        return max_len
+
 # 整数反转
 # 注意事项 不能越界 注意正反
 class Solution:
--- practice.py
+++ practice.py
@@ -6,54 +6,7 @@ Time             :2021/01/08 16:58:37
 Author           :tab gui
 Version          :1.0
 '''
-
-# 1题
-# 暴力法
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
-# hashmap
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}
-        for i in range(len(nums)):
-            if hashmap.get(target - nums[i]) is not None: # 返回对应的索引
-                return [i, hashmap.get(target - nums[i])]
-            else:
-                hashmap[nums[i]] = i
-
-# 2题 链表相加
-# 思路 比较简单 注意创建链表之后的操作需要用新变量替代进行操作,否则一直是空链表
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        re = ListNode(0)
-        r = re
-        carry = 0
-        while(l1 or l2):
-            a = l1.val if l1 else 0
-            b = l2.val if l2 else 0
-            s = a + b + carry
-            carry = s//10
-            r.next = ListNode(s%10)
-            print(re)
-            print(r)
-            r = r.next
-            if(l1!=None):
-                l1 = l1.next
-            if(l2!=None):
-                l2 = l2.next
-        if(carry>0):
-            r.next = ListNode(1)
-        return re.next
-                
+# 
 # 动态规划部分
 
 # 72题
